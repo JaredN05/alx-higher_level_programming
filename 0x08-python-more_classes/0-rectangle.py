ac82949c521abd073f4e7cefec652a929a12e554
JaredN05 <jaredleenaicker@gmail.com>
2024-01-04 09:21:17 -0800

    adding first file and README file

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Task: 0. Simple rectangle
+# Task Completed By: Jared Naicker
+# File Name: 0-rectangle.py
+"""Module for defining a Rectangle class."""
+class Rectangle:
+    """A basic class for rectangles."""
+    pass
