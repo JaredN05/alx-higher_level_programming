468710a36425bd810485d74ef3d9983908f386f2
JaredN05 <jaredleenaicker@gmail.com>
2024-01-04 09:47:05 -0800

    adding file with fixes

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-# Task: 0. Simple rectangle
-# Task Completed By: Jared Naicker
-# File Name: 0-rectangle.py
 """Module for defining a Rectangle class."""
 class Rectangle:
     """A basic class for rectangles."""
